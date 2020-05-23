bd6a49589766b9fdca08c5df9440423504fe8ffd
a-rosenfeld <65213820+a-rosenfeld@users.noreply.github.com>
2020-05-23 09:56:12 -0400

    Total Flights, Arrival+15 min, Weather Delay
    Expected Output:
    Total Flights
    >ATL Flights: 1476
    >CLT Flights: 476
    >MIA Flights: 205
    15 min Arrival Delay*
    >15 min Arrival Delay to ATL: 262
    >15 min Arrival Delay to CLT: 90
    >15 min Arrival Delay to MIA: 33
    Weather Delay*
    >Weather Delay to ATL: 15
    >Weather Delay to CLT: 3
    >Weather Delay to MIA: 0
    *please note this data was not recorded for all flights

--- question_two.sh
+++ question_two.sh
@@ -24,3 +24,89 @@ done <flight.txt
 echo "ATL Flights: $ATL_flights"
 echo "CLT Flights: $CLT_flights"
 echo "MIA Flights: $MIA_flights"
+
+
+ATL_arriv_delay=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'ATL' | awk '{print $3}' > ATL_arriv_delay.txt
+
+while read i
+do
+if [ "$i" == "1.00" ]
+then
+ATL_arriv_delay=$((ATL_arriv_delay+1))
+fi
+done < ATL_arriv_delay.txt
+
+echo "15 min Arrival Delay to ATL: $ATL_arriv_delay"
+
+CLT_arriv_delay=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'CLT' | awk '{print $3}' > CLT_arriv_delay.txt
+
+while read i
+do
+if [ "$i" == "1.00" ]
+then
+CLT_arriv_delay=$((CLT_arriv_delay+1))
+fi
+done < CLT_arriv_delay.txt
+
+echo "15 min Arrival Delay to CLT: $CLT_arriv_delay"
+
+MIA_arriv_delay=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'MIA' | awk '{print $3}' > MIA_arriv_delay.txt
+
+while read i
+do
+if [ "$i" == "1.00" ]
+then
+MIA_arriv_delay=$((MIA_arriv_delay+1))
+fi
+done < MIA_arriv_delay.txt
+
+echo "15 min Arrival Delay to MIA: $MIA_arriv_delay"
+
+
+ATL_weather=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'ATL' | awk '{print $4}' | grep -E [^0.00] > ATL_weather.txt
+
+while read i
+do
+if [ "$i" != "0.00" ]
+then
+ATL_weather=$((ATL_weather+1))
+fi
+done < ATL_weather.txt
+
+echo "Weather Delay to ATL: $ATL_weather"
+
+CLT_weather=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'CLT' | awk '{print $4}' | grep -E [^0.00] > CLT_weather.txt
+
+while read i
+do
+if [ "$i" != "0.00" ]
+then
+CLT_weather=$((CLT_weather+1))
+fi
+done < CLT_weather.txt
+
+echo "Weather Delay to CLT: $CLT_weather"
+
+MIA_weather=0
+
+grep GNV /ufrc/bsc4452/share/Class_Files/data/flights.May2017-Apr2018.csv | awk 'BEGIN {FS=","} {print $7,$13,$16,$24}' | grep -E 'MIA' | awk '{print $4}' | grep -E [^0.00] > MIA_weather.txt
+
+while read i
+do
+if [ "$i" != "0.00" ]
+then
+MIA_weather=$((MIA_weather+1))
+fi
+done < MIA_weather.txt
+
+echo "Weather Delay to MIA: $MIA_weather"
